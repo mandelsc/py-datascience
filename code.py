63ab2d11cd8abd5fb53b941b84b640c4228c121e
Scott Mandel <scottmandel@gmail.com>
2015-06-30 15:48:49 -0500

    updated again

--- code.py
+++ code.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 def add(x, y):
     return x + y;
 
@@ -6,13 +5,3 @@ def increment(x):
     return add(x, 1);
 
 def hello():
-=======
-def add(x, y):
-    return x + y;
-
-def increment(x):
-    return add(x, 1);
-
-def hello():
->>>>>>> 9bb5383463d6e5ee8e4f4ff40a3bf85c7fb349b2
-    return "world";
