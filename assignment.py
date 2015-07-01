fd5d39706e7d402099b5db5e6d00a7b891ded6a3
Nishanth Samala <nishanth.samala@gmail.com>
2015-06-30 23:27:32 -0700

    added some clarification to the problems for lesson 1

--- assignment.py
+++ assignment.py
@@ -1,5 +1,10 @@
 import csv
 
+# In this assignment, tests.py will use the read_file function listed below
+# and provide you with the data for each of the problems. Be sure to review
+# how to access data fields by column header from the lesson in order to
+# to complete the assignment.
+
 def read_file(filename):
 	lines = []
 	with open(filename, 'rb') as csvfile:
@@ -17,22 +22,22 @@ def prob_01(data):
 	
 	return count
 
-# Get the name of the first player in the file
+# Get the name of the first player in the file using the column name (PLAYER FULL NAME)
 def prob_02(data):
 	
 	return name
 	
-# Get the date on the last line in the file
+# Get the date on the last line in the file using the column name (DATE)
 def prob_03(data):
 	
 	return date
 	
-# Get the 100th player's name
+# Get the 100th player's name using the column name (PLAYER FULL NAME)
 def prob_04(data):
 	
 	return name
 
-# Get a list of the unique OWN_TEAM elements in the file
+# Get an array of the unique OWN_TEAM elements in the file
 def prob_05(data):
 	teams = []
 	
